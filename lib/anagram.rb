782860298f43fc71f1a11a6202ad31b95889fc72
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-31 14:45:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,11 @@ class Anagram
   def match(array)
     new_array = []
     if @words.split("").sort == array.split("").sort
-      array
+      new_array << array
+      new_array
+    else
+      new_array
+    end
     
   end
 end
